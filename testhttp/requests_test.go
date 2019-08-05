927720eabed45547fcfe2ec1c2fa27887653be06
Bartłomiej Kiełbasa <bartlomiej.kielbasa@brainly.com>
2019-08-05 09:38:35 +0200

    add tests to testhttp package

--- testhttp/requests_test.go
+++ testhttp/requests_test.go
@@ -1,6 +1,29 @@
 package testhttp
 
-import "testing"
+import (
+	"bytes"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestValidMethods(t *testing.T) {
+	handler := TestHTTP{
+		handler: testHandler{},
+	}
+
+	methods := []string{"Get", "Post", "Trace", "Options", "Head", "Connect", "Patch", "Put", "Delete"}
+
+	for _, method := range methods {
+		h := reflect.ValueOf(&handler)
+		m := h.MethodByName(method)
+		body := bytes.NewReader(nil)
+		values := m.Call([]reflect.Value{reflect.ValueOf(""), reflect.ValueOf(body)})
+		if !values[1].IsNil() {
+			t.Errorf("the function should not return an error")
+		}
+	}
+}
 
 func TestInvalidMethod(t *testing.T) {
 	method := "NOT EXISTS"
@@ -9,4 +32,10 @@ func TestInvalidMethod(t *testing.T) {
 	if err == nil {
 		t.Error("the handler should not allow for invalid HTTP methods")
 	}
+}
+
+type testHandler struct {
+}
+
+func (h testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
