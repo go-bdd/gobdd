e1c6858ade53868dc0b5f2e86f62c7f7c611bd27
Bartłomiej Kiełbasa <bartlomiej.kielbasa@brainly.com>
2020-02-04 14:24:21 +0100

    more docs

--- context/context.go
+++ context/context.go
@@ -4,23 +4,26 @@ import (
 	"fmt"
 )
 
-// Holds two types of data:
-//
-// * data saved by previously executed steps
+// Holds data from previously executed steps
 type Context struct {
 	values map[interface{}]interface{}
 }
 
+// Creates a new (empty) context struct
 func New() Context {
 	return Context{
 		values: map[interface{}]interface{}{},
 	}
 }
 
+// Sets the value under the key
 func (ctx Context) Set(key interface{}, value interface{}) {
 	ctx.values[key] = value
 }
 
+// Returns the data under the key.
+// If couldn't find anything but the default value is provided, returns the default value.
+// Otherwise, it panics.
 func (ctx Context) Get(key interface{}, defaultValue ...interface{}) interface{} {
 	if _, ok := ctx.values[key]; !ok {
 		if len(defaultValue) == 1 {
