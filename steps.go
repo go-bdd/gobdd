73e92c1d52cd0d1266df59e93df61b69c3e9b0bc
Márk Sági-Kazár <sagikazarmark@users.noreply.github.com>
2020-05-21 15:26:54 +0200

    Remove context from step return values (#103)

--- steps.go
+++ steps.go
@@ -13,22 +13,11 @@ func validateStepFunc(f interface{}) error {
 		return errors.New("the parameter should be a function")
 	}
 
-	if value.Type().NumOut() != 1 {
-		return errors.New("the function should return the context.Context")
-	}
-
-	val := value.Type().Out(0)
-
-	n := val.ConvertibleTo(reflect.TypeOf((*context.Context)(nil)).Elem())
-	if !n {
-		return errors.New("the returned value should implement the context.Context interface")
-	}
-
 	if value.Type().NumIn() < 2 {
 		return errors.New("the function should have StepTest and Context as the first argument")
 	}
 
-	val = value.Type().In(0)
+	val := value.Type().In(0)
 
 	testingInterface := reflect.TypeOf((*StepTest)(nil)).Elem()
 	if !val.Implements(testingInterface) {
@@ -37,7 +26,7 @@ func validateStepFunc(f interface{}) error {
 
 	val = value.Type().In(1)
 
-	n = val.ConvertibleTo(reflect.TypeOf((*context.Context)(nil)).Elem())
+	n := val.ConvertibleTo(reflect.TypeOf((*context.Context)(nil)).Elem())
 	if !n {
 		return errors.New("the function should have Context as the second argument")
 	}
